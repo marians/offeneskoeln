27b5f8f9ccb4a33fc0593c8ee37cb3412680373b
Marian Steinbach <marian@sendung.de>
2012-03-09 14:49:12 +0100

    Whitespace und Kommentare

--- scripts/config_dist.py
+++ scripts/config_dist.py
@@ -2,28 +2,31 @@
 # encoding: utf-8
 
 """
-    Konfiguration für Python-basierte Scripte
-    
-    Copyright (c) 2012 Marian Steinbach
+Konfiguration für Python-basierte Scripte
 
-    Hiermit wird unentgeltlich jeder Person, die eine Kopie der Software und
-    der zugehörigen Dokumentationen (die "Software") erhält, die Erlaubnis 
-    erteilt, sie uneingeschränkt zu benutzen, inklusive und ohne Ausnahme, dem 
-    Recht, sie zu verwenden, kopieren, ändern, fusionieren, verlegen, 
-    verbreiten, unterlizenzieren und/oder zu verkaufen, und Personen, die diese 
-    Software erhalten, diese Rechte zu geben, unter den folgenden Bedingungen:
+Die Einstellungen in dieser Datei müssen der jeweiligen Umgebung angepasst
+und der Name der Datei zu "config.py" geändert werden.
 
-    Der obige Urheberrechtsvermerk und dieser Erlaubnisvermerk sind in allen 
-    Kopien oder Teilkopien der Software beizulegen.
+Copyright (c) 2012 Marian Steinbach
 
-    Die Software wird ohne jede ausdrückliche oder implizierte Garantie 
-    bereitgestellt, einschließlich der Garantie zur Benutzung für den 
-    vorgesehenen oder einen bestimmten Zweck sowie jeglicher Rechtsverletzung, 
-    jedoch nicht darauf beschränkt. In keinem Fall sind die Autoren oder 
-    Copyrightinhaber für jeglichen Schaden oder sonstige Ansprüche haftbar zu 
-    machen, ob infolge der Erfüllung eines Vertrages, eines Delikts oder anders 
-    im Zusammenhang mit der Software oder sonstiger Verwendung der Software 
-    entstanden.
+Hiermit wird unentgeltlich jeder Person, die eine Kopie der Software und
+der zugehörigen Dokumentationen (die "Software") erhält, die Erlaubnis
+erteilt, sie uneingeschränkt zu benutzen, inklusive und ohne Ausnahme, dem
+Recht, sie zu verwenden, kopieren, ändern, fusionieren, verlegen,
+verbreiten, unterlizenzieren und/oder zu verkaufen, und Personen, die diese
+Software erhalten, diese Rechte zu geben, unter den folgenden Bedingungen:
+
+Der obige Urheberrechtsvermerk und dieser Erlaubnisvermerk sind in allen
+Kopien oder Teilkopien der Software beizulegen.
+
+Die Software wird ohne jede ausdrückliche oder implizierte Garantie
+bereitgestellt, einschließlich der Garantie zur Benutzung für den
+vorgesehenen oder einen bestimmten Zweck sowie jeglicher Rechtsverletzung,
+jedoch nicht darauf beschränkt. In keinem Fall sind die Autoren oder
+Copyrightinhaber für jeglichen Schaden oder sonstige Ansprüche haftbar zu
+machen, ob infolge der Erfüllung eines Vertrages, eines Delikts oder anders
+im Zusammenhang mit der Software oder sonstiger Verwendung der Software
+entstanden.
 """
 
 # Basis-URL für Solr Webservice
