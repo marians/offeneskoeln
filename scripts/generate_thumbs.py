d42945a884eea56fdccdd72e2b00a8d5a9a6897f
Marian Steinbach <marian@sendung.de>
2012-03-09 14:29:35 +0100

    whitespace

--- scripts/generate_thumbs.py
+++ scripts/generate_thumbs.py
@@ -2,32 +2,32 @@
 # encoding: utf-8
 
 """
-    Generiert Thumbnails von allen Attachments
-    
-    TODO:
-    - Prüfen, ob Dokument neuer ist als Thumbnail und ggf. Thumb neu produzieren.
-    - Nur bei bestimmten Dateiendungen Thumbnail erzeugen
-    
-    Copyright (c) 2012 Marian Steinbach
+Generiert Thumbnails von allen Attachments
 
-    Hiermit wird unentgeltlich jeder Person, die eine Kopie der Software und
-    der zugehörigen Dokumentationen (die "Software") erhält, die Erlaubnis 
-    erteilt, sie uneingeschränkt zu benutzen, inklusive und ohne Ausnahme, dem 
-    Recht, sie zu verwenden, kopieren, ändern, fusionieren, verlegen, 
-    verbreiten, unterlizenzieren und/oder zu verkaufen, und Personen, die diese 
-    Software erhalten, diese Rechte zu geben, unter den folgenden Bedingungen:
+TODO:
+- Prüfen, ob Dokument neuer ist als Thumbnail und ggf. Thumb neu produzieren.
+- Nur bei bestimmten Dateiendungen Thumbnail erzeugen
 
-    Der obige Urheberrechtsvermerk und dieser Erlaubnisvermerk sind in allen 
-    Kopien oder Teilkopien der Software beizulegen.
+Copyright (c) 2012 Marian Steinbach
 
-    Die Software wird ohne jede ausdrückliche oder implizierte Garantie 
-    bereitgestellt, einschließlich der Garantie zur Benutzung für den 
-    vorgesehenen oder einen bestimmten Zweck sowie jeglicher Rechtsverletzung, 
-    jedoch nicht darauf beschränkt. In keinem Fall sind die Autoren oder 
-    Copyrightinhaber für jeglichen Schaden oder sonstige Ansprüche haftbar zu 
-    machen, ob infolge der Erfüllung eines Vertrages, eines Delikts oder anders 
-    im Zusammenhang mit der Software oder sonstiger Verwendung der Software 
-    entstanden.
+Hiermit wird unentgeltlich jeder Person, die eine Kopie der Software und
+der zugehörigen Dokumentationen (die "Software") erhält, die Erlaubnis
+erteilt, sie uneingeschränkt zu benutzen, inklusive und ohne Ausnahme, dem
+Recht, sie zu verwenden, kopieren, ändern, fusionieren, verlegen,
+verbreiten, unterlizenzieren und/oder zu verkaufen, und Personen, die diese
+Software erhalten, diese Rechte zu geben, unter den folgenden Bedingungen:
+
+Der obige Urheberrechtsvermerk und dieser Erlaubnisvermerk sind in allen
+Kopien oder Teilkopien der Software beizulegen.
+
+Die Software wird ohne jede ausdrückliche oder implizierte Garantie
+bereitgestellt, einschließlich der Garantie zur Benutzung für den
+vorgesehenen oder einen bestimmten Zweck sowie jeglicher Rechtsverletzung,
+jedoch nicht darauf beschränkt. In keinem Fall sind die Autoren oder
+Copyrightinhaber für jeglichen Schaden oder sonstige Ansprüche haftbar zu
+machen, ob infolge der Erfüllung eines Vertrages, eines Delikts oder anders
+im Zusammenhang mit der Software oder sonstiger Verwendung der Software
+entstanden.
 """
 
 import config
@@ -48,7 +48,6 @@ CONVERT_PATH = '/usr/bin/convert'
 VALID_TYPES = ['jpg', 'pdf', 'tif', 'bmp', 'png', 'gif']
 
 # Diese Größen (Höhe) werden generiert:
-SIZES = [300, 600, 150]
 SIZES = [300, 800, 150]
 
 THUMB_SUFFIX = 'jpg'
