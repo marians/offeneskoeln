3864128b294afcc146d4d35addbb2d078ba1cda7
Marian Steinbach <marian@sendung.de>
2012-09-06 10:59:53 +0200

    Neues Script (noch unfertig): Download von Attachments für einen bestimmten Datumsbereich
    Wenn es fertig ist, soll man mit diesem Script schnell die Attachments
    aller Dokumente eines bestimmten Datumsbereichs herunter laden können.
    
    Wegen eines Fehlers in der API (issue #54) kann das Script seine
    Funktion noch nicht erfüllen, ist daher noch nicht fertig geschrieben.

---
+++ scripts/download_attachments.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+Lädt Anhänge bis zu einem gewissen Veröffentlichungsdatum herunter
+
+Copyright (c) 2012 Marian Steinbach
+
+Hiermit wird unentgeltlich jeder Person, die eine Kopie der Software und
+der zugehörigen Dokumentationen (die "Software") erhält, die Erlaubnis
+erteilt, sie uneingeschränkt zu benutzen, inklusive und ohne Ausnahme, dem
+Recht, sie zu verwenden, kopieren, ändern, fusionieren, verlegen,
+verbreiten, unterlizenzieren und/oder zu verkaufen, und Personen, die diese
+Software erhalten, diese Rechte zu geben, unter den folgenden Bedingungen:
+
+Der obige Urheberrechtsvermerk und dieser Erlaubnisvermerk sind in allen
+Kopien oder Teilkopien der Software beizulegen.
+
+Die Software wird ohne jede ausdrückliche oder implizierte Garantie
+bereitgestellt, einschließlich der Garantie zur Benutzung für den
+vorgesehenen oder einen bestimmten Zweck sowie jeglicher Rechtsverletzung,
+jedoch nicht darauf beschränkt. In keinem Fall sind die Autoren oder
+Copyrightinhaber für jeglichen Schaden oder sonstige Ansprüche haftbar zu
+machen, ob infolge der Erfüllung eines Vertrages, eines Delikts oder anders
+im Zusammenhang mit der Software oder sonstiger Verwendung der Software
+entstanden.
+"""
+
+import sys
+import json
+import urllib
+from optparse import OptionParser
+
+
+def get_documents(date=None):
+    """
+    Führt den API-Request für das Abrufen von Dokumenten aus
+    """
+    url = 'http://offeneskoeln.de/api/documents?docs=10000&output=attachments&date=%s' % date
+    request = urllib.urlopen(url)
+    if request.getcode() != '200':
+        sys.stderr.write('Bad HTTP Status: ' + str(request.getcode()))
+        sys.stderr.write(request.read())
+    else:
+        jsons = request.read()
+        return json.loads(jsons)
+
+if __name__ == '__main__':
+    usage = "usage: %prog <daterange>\n\nWhere daterange is e.g. 2010-2011 or 201208-201209"
+    parser = OptionParser(usage=usage)
+    (options, args) = parser.parse_args()
+    if len(args) != 1:
+        parser.error("No date range given")
+    docs = get_documents(args[0])
+    print docs
+
