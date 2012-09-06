8ee4dfdba556ad0f0163b64f8dd72349e346867e
Marian Steinbach <marian@sendung.de>
2012-09-06 11:59:53 +0200

    Whitespace

--- webapp/config_dist.py
+++ webapp/config_dist.py
@@ -2,33 +2,33 @@
 # encoding: utf-8
 
 """
-    Konfigurationsdatei für die Web-Applikation.
-    
-    Die Datei wird im github Repository als config_dist.py ausgeliefert. Sie muss in
-    config.py umbenannt und auf die lokalen Bedingungen angepasst werden.
-    
-    Mehr unter https://github.com/marians/offeneskoeln/wiki/Leitfaden-für-Entwickler
-    
-    Copyright (c) 2012 Marian Steinbach
+Konfigurationsdatei für die Web-Applikation.
 
-    Hiermit wird unentgeltlich jeder Person, die eine Kopie der Software und
-    der zugehörigen Dokumentationen (die "Software") erhält, die Erlaubnis 
-    erteilt, sie uneingeschränkt zu benutzen, inklusive und ohne Ausnahme, dem 
-    Recht, sie zu verwenden, kopieren, ändern, fusionieren, verlegen, 
-    verbreiten, unterlizenzieren und/oder zu verkaufen, und Personen, die diese 
-    Software erhalten, diese Rechte zu geben, unter den folgenden Bedingungen:
+Die Datei wird im github Repository als config_dist.py ausgeliefert. Sie muss in
+config.py umbenannt und auf die lokalen Bedingungen angepasst werden.
 
-    Der obige Urheberrechtsvermerk und dieser Erlaubnisvermerk sind in allen 
-    Kopien oder Teilkopien der Software beizulegen.
+Mehr unter https://github.com/marians/offeneskoeln/wiki/Leitfaden-für-Entwickler
 
-    Die Software wird ohne jede ausdrückliche oder implizierte Garantie 
-    bereitgestellt, einschließlich der Garantie zur Benutzung für den 
-    vorgesehenen oder einen bestimmten Zweck sowie jeglicher Rechtsverletzung, 
-    jedoch nicht darauf beschränkt. In keinem Fall sind die Autoren oder 
-    Copyrightinhaber für jeglichen Schaden oder sonstige Ansprüche haftbar zu 
-    machen, ob infolge der Erfüllung eines Vertrages, eines Delikts oder anders 
-    im Zusammenhang mit der Software oder sonstiger Verwendung der Software 
-    entstanden.
+Copyright (c) 2012 Marian Steinbach
+
+Hiermit wird unentgeltlich jeder Person, die eine Kopie der Software und
+der zugehörigen Dokumentationen (die "Software") erhält, die Erlaubnis
+erteilt, sie uneingeschränkt zu benutzen, inklusive und ohne Ausnahme, dem
+Recht, sie zu verwenden, kopieren, ändern, fusionieren, verlegen,
+verbreiten, unterlizenzieren und/oder zu verkaufen, und Personen, die diese
+Software erhalten, diese Rechte zu geben, unter den folgenden Bedingungen:
+
+Der obige Urheberrechtsvermerk und dieser Erlaubnisvermerk sind in allen
+Kopien oder Teilkopien der Software beizulegen.
+
+Die Software wird ohne jede ausdrückliche oder implizierte Garantie
+bereitgestellt, einschließlich der Garantie zur Benutzung für den
+vorgesehenen oder einen bestimmten Zweck sowie jeglicher Rechtsverletzung,
+jedoch nicht darauf beschränkt. In keinem Fall sind die Autoren oder
+Copyrightinhaber für jeglichen Schaden oder sonstige Ansprüche haftbar zu
+machen, ob infolge der Erfüllung eines Vertrages, eines Delikts oder anders
+im Zusammenhang mit der Software oder sonstiger Verwendung der Software
+entstanden.
 """
 
 import web
@@ -59,7 +59,7 @@ HTTP_SESSION_TABLE_NAME = 'webapp_sessiondata'
 SESSION_SECRET_KEY = 'xxxxxxxxxxxxxxxxx'
 
 # Schlüssel der Yahoo! Placefinder App
-# Besorg Dir zum Entwickeln einen eigenen unter 
+# Besorg Dir zum Entwickeln einen eigenen unter
 # http://developer.yahoo.com/geo/placefinder/
 PLACEFINDER_APP_ID = 'xxxxxxxx'
 
