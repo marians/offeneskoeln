21525a89c5042da4628fe56244dbd2fd65042cd4
Marian Steinbach <marian@sendung.de>
2012-10-10 19:06:45 +0200

    Whitespace

--- scripts/update_thumbs_data.py
+++ scripts/update_thumbs_data.py
@@ -2,58 +2,72 @@
 # encoding: utf-8
 
 """
-    Schreibt Daten über Attachment-Thumbnails in die Datenbank
-    
-    Copyright (c) 2012 Marian Steinbach
+Schreibt Daten über Attachment-Thumbnails in die Datenbank
 
-    Hiermit wird unentgeltlich jeder Person, die eine Kopie der Software und
-    der zugehörigen Dokumentationen (die "Software") erhält, die Erlaubnis 
-    erteilt, sie uneingeschränkt zu benutzen, inklusive und ohne Ausnahme, dem 
-    Recht, sie zu verwenden, kopieren, ändern, fusionieren, verlegen, 
-    verbreiten, unterlizenzieren und/oder zu verkaufen, und Personen, die diese 
-    Software erhalten, diese Rechte zu geben, unter den folgenden Bedingungen:
+Für jede existierende Thumbnail-Datei wird ein Eintrag vorgenommen.
 
-    Der obige Urheberrechtsvermerk und dieser Erlaubnisvermerk sind in allen 
-    Kopien oder Teilkopien der Software beizulegen.
+TODO:
+- Check in umgekehrte Richtung: Prüfen, welche Einträge aus der
+  Datenbank gelöscht werden können, weil die Thumbnail-Datei nicht
+  mehr existiert.
+"""
 
-    Die Software wird ohne jede ausdrückliche oder implizierte Garantie 
-    bereitgestellt, einschließlich der Garantie zur Benutzung für den 
-    vorgesehenen oder einen bestimmten Zweck sowie jeglicher Rechtsverletzung, 
-    jedoch nicht darauf beschränkt. In keinem Fall sind die Autoren oder 
-    Copyrightinhaber für jeglichen Schaden oder sonstige Ansprüche haftbar zu 
-    machen, ob infolge der Erfüllung eines Vertrages, eines Delikts oder anders 
-    im Zusammenhang mit der Software oder sonstiger Verwendung der Software 
-    entstanden.
+"""
+Copyright (c) 2012 Marian Steinbach
+
+Hiermit wird unentgeltlich jeder Person, die eine Kopie der Software und
+der zugehörigen Dokumentationen (die "Software") erhält, die Erlaubnis
+erteilt, sie uneingeschränkt zu benutzen, inklusive und ohne Ausnahme, dem
+Recht, sie zu verwenden, kopieren, ändern, fusionieren, verlegen,
+verbreiten, unterlizenzieren und/oder zu verkaufen, und Personen, die diese
+Software erhalten, diese Rechte zu geben, unter den folgenden Bedingungen:
+
+Der obige Urheberrechtsvermerk und dieser Erlaubnisvermerk sind in allen
+Kopien oder Teilkopien der Software beizulegen.
+
+Die Software wird ohne jede ausdrückliche oder implizierte Garantie
+bereitgestellt, einschließlich der Garantie zur Benutzung für den
+vorgesehenen oder einen bestimmten Zweck sowie jeglicher Rechtsverletzung,
+jedoch nicht darauf beschränkt. In keinem Fall sind die Autoren oder
+Copyrightinhaber für jeglichen Schaden oder sonstige Ansprüche haftbar zu
+machen, ob infolge der Erfüllung eines Vertrages, eines Delikts oder anders
+im Zusammenhang mit der Software oder sonstiger Verwendung der Software
+entstanden.
 """
 
 
 import config
 import os
 import re
-import sys
 import Image
 import MySQLdb
 
-# Schreibt Info über Thumbnails in die Datenbank
-
-# Tabelle: config.DB_THUMBS_TABLE
 
 def find_thumbs(thumbs_folder):
+    """
+    Durchsucht durch den Thumbnail-Ordner
+    """
     for root, dirs, files in os.walk(thumbs_folder):
         for fname in files:
-            #print fname
             file_id = get_id_from_file(fname)
             if file_id is not None:
                 path = root + '/' + fname
                 write_to_db(file_id, fname, path)
-            
-# returns numeric ID part from attachment file name
+
+
 def get_id_from_file(filename):
+    """
+    Gibt die numerische Attachment-ID innerhalb des Dateinamens zurück
+    """
     m = re.match(r'([0-9]+)[-0-9]*\.[a-z]+', filename)
     if m is not None:
         return m.group(1)
 
+
 def write_to_db(file_id, filename, path):
+    """
+    Schreibt einen Thumbnail-Eintrag in die Datenbank
+    """
     global cursor
     im = Image.open(path)
     im.size[0], im.size[1]
@@ -63,14 +77,14 @@ def write_to_db(file_id, filename, path):
     if m is not None:
         page = m.group(1)
     print file_id, filename, im.size[0], im.size[1], page
-    sql = """INSERT INTO """ + config.DB_THUMBS_TABLE + """ 
+    sql = """INSERT INTO """ + config.DB_THUMBS_TABLE + """
         (attachment_id, filename, width, height, page) VALUES
         (%s, %s, %s, %s, %s)
         ON DUPLICATE KEY UPDATE filename=%s, width=%s, height=%s, page=%s"""
     cursor.execute(sql, (file_id, filename, im.size[0], im.size[1], page, filename, im.size[0], im.size[1], page))
 
+
 if __name__ == '__main__':
-    conn = MySQLdb.connect (host=config.DB_HOST, user=config.DB_USER, passwd=config.DB_PASS, db=config.DB_NAME)
+    conn = MySQLdb.connect(host=config.DB_HOST, user=config.DB_USER, passwd=config.DB_PASS, db=config.DB_NAME)
     cursor = conn.cursor(MySQLdb.cursors.DictCursor)
     find_thumbs(config.THUMBS_PATH)
-    
