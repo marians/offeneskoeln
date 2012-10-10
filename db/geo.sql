8bb21d66d5dc6927bdcecc11d006d2388e7ded01
Marian Steinbach <marian@sendung.de>
2012-10-10 16:22:51 +0200

    Unnötiges IF NOT EXISTS entfernt

--- db/geo.sql
+++ db/geo.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `geo_nodes` (
+CREATE TABLE `geo_nodes` (
   `id` int(11) unsigned NOT NULL auto_increment,
   `latitude` decimal(10,7) default NULL,
   `longitude` decimal(10,7) default NULL,
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `geo_nodes` (
   KEY `latitudelongitude` (`latitude`,`longitude`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `geo_objects` (
+CREATE TABLE `geo_objects` (
   `id` int(11) unsigned NOT NULL auto_increment,
   `name` varchar(255) default NULL,
   `type` varchar(100) default NULL,
@@ -15,10 +15,11 @@ CREATE TABLE IF NOT EXISTS `geo_objects` (
   KEY `nameindex` (`name`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `geo_objects2nodes` (
+CREATE TABLE `geo_objects2nodes` (
   `object_id` int(11) unsigned NOT NULL,
   `node_id` int(11) unsigned NOT NULL,
   UNIQUE KEY `quickfind` (`object_id`,`node_id`),
   KEY `object_id` (`object_id`),
   KEY `node_id` (`node_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
