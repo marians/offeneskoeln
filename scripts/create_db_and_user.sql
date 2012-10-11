63ac887263fbad8e4b96aedd1e5b7d4d3742e65c
Marian Steinbach <marian@sendung.de>
2012-10-11 10:38:35 +0200

    Bugfix: no quotes allowed

--- scripts/create_db_and_user.sql
+++ scripts/create_db_and_user.sql
@@ -1,4 +1,4 @@
 
-CREATE DATABASE 'offeneskoeln';
+CREATE DATABASE offeneskoeln;
 CREATE USER 'offeneskoeln'@'localhost' IDENTIFIED BY '';
 GRANT ALL ON offeneskoeln.* TO 'offeneskoeln'@'localhost' IDENTIFIED BY '';
