7e5f20152081056af9912fa2c0bfbf5340aadac5
Marian Steinbach <marian@sendung.de>
2013-04-09 13:15:08 +0200

    Added MongoDBSessionInterface

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask.ext.pymongo import PyMongo
 
+from flask.ext.mongo_sessions import MongoDBSessionInterface
+
 app = Flask(__name__)
 app.config.from_pyfile('../config.py')
 mongo = PyMongo(app)
+with app.app_context():
+    app.session_interface = MongoDBSessionInterface(app, mongo.db, 'flasksessions')
 
 import webapp.views
 import webapp.api
