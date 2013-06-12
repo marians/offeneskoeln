1cb9f6078d6aa29f262e29d9a43072b691be3125
the-infinity <mail@ernestoruge.de>
2013-06-12 02:08:36 +0200

    Bugfix

--- cron.daily.sh
+++ cron.daily.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Bochum
+cd /opt/ris-scraper/
 sudo -u ris-scraper /opt/ris-scraper/ris-scraper/bin/python /opt/ris-scraper/main.py -c bochum --start 2013-01 --end 2013-12
+cd /opt/ris-web/
 sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_fulltext.py bochum
 sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_georeferences.py bochum
 sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_thumbs.py bochum
@@ -9,7 +11,9 @@ sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/elasticsear
 
 
 # Moers
+cd /opt/ris-scraper/
 sudo -u ris-scraper /opt/ris-scraper/ris-scraper/bin/python /opt/ris-scraper/main.py -c moers --start 2013-01 --end 2013-12
+cd /opt/ris-web/
 sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_fulltext.py moers
 sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_georeferences.py moers
 sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_thumbs.py moers
