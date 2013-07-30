11a6929f5d673aade67252c6fc4d64c3f4969252
Ernesto Ruge <mail@ernestoruge.de>
2013-07-30 21:23:17 +0200

    Better Cronjob, NFS Support

--- cron.daily.sh
+++ cron.daily.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-# Bochum
-cd /opt/ris-scraper/
-sudo -u ris-scraper /opt/ris-scraper/ris-scraper/bin/python /opt/ris-scraper/main.py -c bochum --start 2013-01 --end 2013-12
-cd /opt/ris-web/
-sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_fulltext.py bochum
-sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_georeferences.py bochum
-sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_thumbs.py bochum
-sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_submissions_rss_feed.py bochum
-sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/elasticsearch_import.py bochum
+#Config
+CITIES=('bochum' 'moers' 'duisburg')
 
+#CDN
+umount /srv/www/static/
+mount /srv/www/static/
 
-# Moers
-cd /opt/ris-scraper/
-sudo -u ris-scraper /opt/ris-scraper/ris-scraper/bin/python /opt/ris-scraper/main.py -c moers --start 2013-01 --end 2013-12
-cd /opt/ris-web/
-sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_fulltext.py moers
-sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_georeferences.py moers
-sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_thumbs.py moers
-sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_submissions_rss_feed.py moers
-sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/elasticsearch_import.py moers
+for CITY in "${CITIES[@]}"
+do
+  cd /opt/ris-scraper/
+  sudo -u ris-scraper /opt/ris-scraper/ris-scraper/bin/python /opt/ris-scraper/main.py -c $CITY --start 2013-06 --end 2013-12
+  cd /opt/ris-web/
+  sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_fulltext.py $CITY
+  sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_georeferences.py $CITY
+  sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_thumbs.py $CITY
+  sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_submissions_rss_feed.py $CITY
+  sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/elasticsearch_import.py $CITY
+done
+
+#CDN
+umount /srv/www/static/
