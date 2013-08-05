011c4b387811d9623876d15155819fc80725e97d
Ernesto Ruge <mail@ernestoruge.de>
2013-08-05 05:01:15 +0200

    URL Generation in Cron

--- cron.daily.sh
+++ cron.daily.sh
@@ -11,6 +11,7 @@ do
   cd /opt/ris-scraper/
   sudo -u ris-scraper /opt/ris-scraper/ris-scraper/bin/python /opt/ris-scraper/main.py -c $CITY --start 2013-06 --end 2013-12
   cd /opt/ris-web/
+  sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_urls.py $CITY
   sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_fulltext.py $CITY
   sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_georeferences.py $CITY
   sudo -u ris-web /opt/ris-web/ris-web/bin/python /opt/ris-web/scripts/generate_thumbs.py $CITY
